268f55e7b6cf8979a5ed850797f0e367c29a0349
mkukke <mkukke@BLRWX10247.internal.imsglobal.com>
2021-02-28 13:12:10 +0530

    Second commit

---
+++ Test/src/TestClass.java
@@ -0,0 +1,15 @@
+
+public class TestClass 
+{
+	public static void main()
+	{
+		Print("Hello");
+
+	}
+
+	private static void Print(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
